4f077ed2f7700a0f1df1e5c8e27bed8e6743fcc8
Busi, Manasa Deepthi <manasadeepthi.busi@swbc.com>
2024-06-13 08:44:40 -0500

    Checking in some Unit tests and code for Saving a Booking.

--- DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
+using Moq;
 
 namespace DeskBooker.Core.Tests.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookingRequestProcessor _processor;
+        private readonly DeskBookingRequest _request;
+        private readonly Mock<IDeskBookingRepository> _deskBookingRepositoryMock;
+
+        public DeskBookingRequestProcessorTests()
+        {
+
+            _request = new DeskBookingRequest
+            {
+                FirstName = "Manasa",
+                LastName = "Busi",
+                Email = "manasa.busi@gmail.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+            _deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
+
+            _processor = new DeskBookingRequestProcessor(
+                _deskBookingRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+
+
+            //Act
+            DeskBookingResult result = _processor.BookDesk(_request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+
+            //Act and Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.Equal("request", exception.ParamName);
+        }
+
+        [Fact]
+        public void ShouldSaveDeskBooking()
+        {
+            DeskBooking savedDeskBooking = null;
+
+            _deskBookingRepositoryMock.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+                .Callback<DeskBooking>(deskbooking =>
+                {
+                    savedDeskBooking = deskbooking;
+                });
+
+            _processor.BookDesk(_request);
+
+            _deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+
+            Assert.NotNull(savedDeskBooking);
+            Assert.Equal(_request.FirstName , savedDeskBooking.FirstName);
+            Assert.Equal(_request.LastName, savedDeskBooking.LastName);
+            Assert.Equal(_request.Email, savedDeskBooking.Email);
+            Assert.Equal(_request.Date, savedDeskBooking.Date);
+
+        }
     }
 }
--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
 
-namespace DeskBooker.Core.Processor
+namespace DeskBooker.Core.Domain
 {
-    internal class DeskBookingRequestProcessor
+    public class DeskBookingRequestProcessor
     {
+        private readonly IDeskBookingRepository _deskBookingRepository;
+
+        public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository)
+        {
+            _deskBookingRepository = deskBookingRepository;
+        }
+
+        public DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            _deskBookingRepository.Save(Create<DeskBooking>(request));
+
+            return Create<DeskBookingResult>(request);
+
+        }
+
+        private static T Create<T>(DeskBookingRequest request) where T : DeskBoookingBase, new()
+        {
+            return new T
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
     }
 }
